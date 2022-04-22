c18e68e822fe0ed7b431733079e4345125d2cbd1
Flávio Ferreira <business.flavioferreira@gmail.com>
2022-04-22 15:17:38 -0300

    initial configs

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/ferreirazdev/fiberapi
+
+go 1.17
+
+require (
+	github.com/gofiber/fiber/v2 v2.32.0
+	github.com/joho/godotenv v1.4.0
+	gorm.io/gorm v1.23.4
+)
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.35.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_book", r.CreateBook)
+	api.Delete("/delete_book/:id", r.DeleteBook)
+	api.Get("/get_book/:id", r.GetBookByID)
+	api.Get("/get_books", r.GetBooks)
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+
+	r := Repository{
+		DB: db,
+	}
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8000")
+}
